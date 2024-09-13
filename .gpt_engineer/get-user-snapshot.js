d389e90eba392dbb447d64af9ff4cccbf26d1904
Streamxyz <qvor.t.todorov@gmail.com>
2024-09-14 02:49:34 +0300

    Update get-user-snapshot.js

--- .gpt_engineer/get-user-snapshot.js
+++ .gpt_engineer/get-user-snapshot.js
@@ -3,8 +3,8 @@ import { toPng } from "html-to-image";
 export const loadGetUserSnapshotEventListener = () => {
   window.addEventListener("blur", () => {
     toPng(document.body).then((url) => {
-      window.top.postMessage({ type: "USER_SNAPSHOT", snapshot: url }, "http://localhost:3000");
-      window.top.postMessage({ type: "USER_SNAPSHOT", snapshot: url }, "https://gptengineer.app");
+      window.top.postMessage({ type: "USER_SNAPSHOT", snapshot: url }, "http://localhost:00");
+      window.top.postMessage({ type: "USER_SNAPSHOT", snapshot: url }, "");
     });
   });
 };
